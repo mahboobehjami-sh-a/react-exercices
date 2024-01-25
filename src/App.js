2062e14937d8bf1da57648f4ac6ae8fbdd27fc99
MahboobehJami <mahboobehjami1380@gmail.com>
2024-01-25 09:55:33 +0330

    import and export in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// برای دیفالت ها میتوان نام دیگری هم اختصاص داد چون فقط یکی از آن وجود دارد اما برای named ها باید حتما عینا همان نام استفاده شود.
+import START_DATE_default,{sessionNumber as SN , COURSE_NAME} from "./tutorial/modules/my-module"
+import * as myModule from "./tutorial/modules/my-module"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h2>
+       Start date : {myModule.default}
+      </h2>
+      <p>
+      Course name : {myModule.COURSE_NAME}
+      </p>
+      <p>
+      Session number : {myModule.sessionNumber}
+      </p>
+      <hr/>
+      <h2>
+       Start date : {START_DATE_default}
+      </h2>
+      <p>
+      Course name : {COURSE_NAME}
+      </p>
+      <p>
+      Session number : {SN}
+      </p>
+      <hr/>
+      <p>Teacher : {myModule.showTeacherInfo()}</p>
+      <p>Course Name : {myModule.course.name}</p>
     </div>
   );
 }
---
+++ src/tutorial/modules/my-module.js
@@ -0,0 +1,30 @@
+// import-export in react
+
+// کامپوننت ها در ری اکت میتوانند متغیر ، ثابت و یا تابع باشند
+const COURSE_NAME = 'Math'
+let sessionNumber = 3
+const START_DATE = '1402-11-05'
+
+export {COURSE_NAME}
+export {sessionNumber}
+export default START_DATE
+
+// فانکشن ها را در زمان نوشتن میتوان اکسپورت نمود اما در متغیر ها نمیتوان ان ها را در زمان تعریف اکسپورت کرد
+
+const teacher = {
+    fisrtName: 'Hamid Reza',
+    lastName: 'Izadi Matin'
+}
+
+export const showTeacherInfo = () => `${teacher.fisrtName} ${teacher.lastName}`
+
+// export function showTeacherInfo() {
+//     return `${teacher.fisrtName} ${teacher.lastName}`
+// }
+
+export const course = {
+    name: COURSE_NAME,
+    currentSession : sessionNumber,
+    startDate: START_DATE,
+    teacher
+}
